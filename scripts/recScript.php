9bbf3031ea25bdf8f8a561533aeb5ad32173dbea
gatesyp <ig11@zips.uakron.edu>
2016-01-30 06:57:28 -0500

    sql statement finished for intersects of likes,dislikes.

--- scripts/recScript.php
+++ scripts/recScript.php
@@ -1,6 +1,13 @@
 <?php
 require 'dbConnect.php';
-$sql = "SELECT first.* FROM profile_data AS first WHERE first.profile = '1022691251124985' and first.response IN (select second.response from profile_data AS second where second.profile != first.profile and second.url = first.url and second.response = first.response)";
+$sql = "SELECT first.profile AS similarPerson, second.response AS sharedResponse, second.url AS sharedURL
+FROM profile_data
+FIRST INNER JOIN profile_data
+SECOND ON second.profile !=  '1022691251124985'
+AND first.profile !=  '1022691251124985'
+AND second.url = first.url
+AND second.response = first.response";
+
 // var_dump($sql);
 // $sql = "SELECT first.* FROM profile_data AS first WHERE first.profile = \'1022691251124985\' and first.response = (select response from profile_data AS second where second.profile != first.profile and second.url = first.url and second.response = first.response)";
   if($result = $conn -> query($sql)){
