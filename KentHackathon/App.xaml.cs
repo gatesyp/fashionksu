4051656a6779f640ca3e292de0bceb2773300a70
Cameron Sinko <csinko@gmail.com>
2016-01-30 13:20:11 -0500

    Removed all of the MVVM stuff

--- KentHackathon/App.xaml.cs
+++ KentHackathon/App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using KentHackathon.ViewModels;
-using KentHackathon.Views;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -15,14 +13,5 @@ namespace KentHackathon
     /// </summary>
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-
-            AppView app = new AppView();
-            AppViewModel context = new AppViewModel();
-            app.DataContext = context;
-            app.Show();
-        }
     }
 }
