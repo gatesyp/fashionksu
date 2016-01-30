f47ce531c33c51d3aff07530e895249ba485a966
gatesyp <ig11@zips.uakron.edu>
2016-01-30 03:35:52 -0500

    SQL statement fix. problem: should output second profile given first

--- scripts/recScript.php
+++ scripts/recScript.php
@@ -1,8 +1,10 @@
 <?php
 require 'dbConnect.php';
-$sql = "SELECT first.* FROM profile_data AS first WHERE first.profile = \'1022691251124985\' and first.response = (select response from profile_data AS second where second.profile != first.profile and second.url = first.url and second.response = first.response)";
+$sql = "SELECT first.* FROM profile_data AS first WHERE first.profile = '1022691251124985' and first.response IN (select second.response from profile_data AS second where second.profile != first.profile and second.url = first.url and second.response = first.response)";
+// var_dump($sql);
+// $sql = "SELECT first.* FROM profile_data AS first WHERE first.profile = \'1022691251124985\' and first.response = (select response from profile_data AS second where second.profile != first.profile and second.url = first.url and second.response = first.response)";
   if($result = $conn -> query($sql)){
-    var_dump($result);
+    var_dump($result -> fetch_object());
     while($row = $result -> fetch_object()){
       $id = $row -> id;
     }
