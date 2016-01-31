116e488d10f90a640dd629d84316f3c9f604bffe
Matt Gates <info@mgates.me>
2016-01-31 07:43:42 -0500

    use amazon aws

--- scripts/dbConnect.php
+++ scripts/dbConnect.php
@@ -1,12 +1,12 @@
 <?php
-$servername = "localhost";
+$servername = "kinect.cijwsnhnawhn.us-east-1.rds.amazonaws.com";
 $username = "root";
-$password = "root";
-$dbname = "fashionksu";
+$password = "lollipop";
+$dbname = "kinect";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+}
