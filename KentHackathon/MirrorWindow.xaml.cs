5109c4dc658c8fff5ee7a82cceb9b92b0a107e42
Cameron Sinko <csinko@gmail.com>
2016-01-31 03:03:18 -0500

    added more kinect stuff

--- KentHackathon/MirrorWindow.xaml.cs
+++ KentHackathon/MirrorWindow.xaml.cs
@@ -121,7 +121,7 @@ namespace KentHackathon
 
             InitializeComponent();
             this.sensorChooser = new KinectSensorChooser();
-            //this.sensorChooser.KinectChanged += SensorChooserOnKinectChanged;
+            this.sensorChooser.KinectChanged += SensorChooserOnKinectChanged;
             this.sensorChooser.Start();
             
             string domain = "https://stoh.io/recScript.php?";
@@ -139,6 +139,51 @@ namespace KentHackathon
             catalog = new Catalog(ArticleList);
 
 
+        }
+
+        private static void SensorChooserOnKinectChanged(object sender, KinectChangedEventArgs args)
+        {
+            if (args.OldSensor != null)
+            {
+                try
+                {
+                    args.OldSensor.DepthStream.Range = DepthRange.Default;
+                    args.OldSensor.SkeletonStream.EnableTrackingInNearRange = false;
+                    args.OldSensor.DepthStream.Disable();
+                    args.OldSensor.SkeletonStream.Disable();
+                }
+                catch (InvalidOperationException)
+                {
+                    // KinectSensor might enter an invalid state while enabling/disabling streams or stream features.
+                    // E.g.: sensor might be abruptly unplugged.
+                }
+            }
+
+            if (args.NewSensor != null)
+            {
+                try
+                {
+                    args.NewSensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+                    args.NewSensor.SkeletonStream.Enable();
+
+                    try
+                    {
+                        args.NewSensor.DepthStream.Range = DepthRange.Near;
+                        args.NewSensor.SkeletonStream.EnableTrackingInNearRange = true;
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        // Non Kinect for Windows devices do not support Near mode, so reset back to default mode.
+                        args.NewSensor.DepthStream.Range = DepthRange.Default;
+                        args.NewSensor.SkeletonStream.EnableTrackingInNearRange = false;
+                    }
+                }
+                catch (InvalidOperationException)
+                {
+                    // KinectSensor might enter an invalid state while enabling/disabling streams or stream features.
+                    // E.g.: sensor might be abruptly unplugged.
+                }
+            }
         }
 
         private void ParseJSON(WebResponse response)
